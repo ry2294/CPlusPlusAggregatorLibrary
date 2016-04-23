251e6b5fc5b14817b8d460a96acf0253cca416f7
ry2294 <ry2294@columbia.edu>
2016-04-23 17:34:41 -0400

    Adding adjacencyMap to graphbuilder

--- AggregatorLibrary/GraphBuilderService/khansgraphbuilder.cpp
+++ AggregatorLibrary/GraphBuilderService/khansgraphbuilder.cpp
@@ -4,8 +4,14 @@ using namespace std;
 using namespace GraphBuilderService;
 
 class KhansGraphBuilder : public Graph {
+private:
+	unordered_map<string, shared_ptr<Task>> adjacencyMap;
 public:
+	KhansGraphBuilder() : adjacencyMap{} {
+	}
+
 	void addVertex(shared_ptr<Runnable> vertex) {
+
 	}
 
 	void constructGraph() {
